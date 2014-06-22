c0c0d0640115ce3fb5b3d9e13b02b81b36311bae
bilal <bilal.hassan@empire.ca>
2014-06-22 14:45:06 -0400

    initial setup of options page

--- ultimate-recent-posts.php
+++ ultimate-recent-posts.php
@@ -2,33 +2,27 @@
 /*
   Plugin Name: Ultimate Recent Posts
   Plugin URI: http://smartcatdesign.net/wp-popup
-  Description: Display your recent posts in many ways. Highly customizable, efficient, and beautiful
+  Description: A highly customizable plugi
   Version: 1.0
   Author: SmartCat, nik-cole
   Author URI: http://smartcatdesign.net
   License: GPL v2
  */
-?>
 
-<?php
-// 1st hook
-// activates the plugin
 register_activation_hook(__FILE__, 'my_plugin_activate');
 
 function my_plugin_activate() {
-    // set redirect to true
-    add_option('urp_activation_redirect', true);
-    urp_popup_register_options();
+    add_option('sc_urp_activation_redirect', true);
+    sc_urp_register_options();
 }
 
-function urp_popup_register_options() {
+function sc_urp_register_options() {
     // declare options array
-    $urp_popup_options = array(
-        'urp_title' => 'Title',
-
+    $sc_urp_options = array(
+        'sc_urp_title' => 'Popup Title',
     );
     // check if option is set, if not, add it
-    foreach ($urp_popup_options as $option_name => $option_value) {
+    foreach ($sc_urp_options as $option_name => $option_value) {
         if (get_option($option_name) === false) {
             add_option($option_name, $option_value);
         } else {
@@ -37,13 +31,26 @@ function urp_popup_register_options() {
     }
 }
 
-// 2nd hook - redirect when activated
-//add_action('admin_init', 'urp_activation_redirect');
-//
-//function urp_activation_redirect() {
-//    if (get_option('urp_activation_redirect', false)) {
-//        delete_option('urp_activation_redirect');
-//        wp_redirect(admin_url() . 'options-general.php?page=wp-popup.php');
-//    }
-//}
+// redirect when activated
+add_action('admin_init', 'sc_urp_activation_redirect');
 
+function sc_urp_activation_redirect() {
+    if (get_option('sc_urp_activation_redirect', false)) {
+        delete_option('sc_urp_activation_redirect');
+        wp_redirect(admin_url() . 'options-general.php?page=urp-options.php');
+    }
+}
+
+add_action('admin_menu', 'sc_urp_menu');
+
+function sc_urp_menu() {
+    add_options_page('Ultimate Recent Posts', 'Ultimate Recent Posts Settings', 'administrator', 'sc_urp_options.php', 'sc_urp_options');
+}
+
+function sc_urp_options(){
+
+
+
+    include_once 'inc/options.php';
+
+}
