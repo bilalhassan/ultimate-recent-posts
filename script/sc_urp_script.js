4fd23415cd6b4596173b124645709e9f8f637e39
bilal <bilal.hassan@empire.ca>
2014-06-22 16:27:23 -0400

    setup basic slider

---
+++ script/sc_urp_script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($){
+
+
+
+    $('#banner-fade').bjqs({
+        height      : 320,
+        width       : 620,
+        responsive  : true
+    });
+
+
+});
--- ultimate-recent-posts.php
+++ ultimate-recent-posts.php
@@ -9,6 +9,7 @@
   License: GPL v2
  */
 
+
 register_activation_hook(__FILE__, 'my_plugin_activate');
 
 function my_plugin_activate() {
@@ -49,6 +50,26 @@ function sc_urp_menu() {
 
 function sc_urp_options(){
     include_once 'inc/options.php';
+
+}
+
+
+// add CSS & JS
+add_action('wp_enqueue_scripts','sc_urp_load_styles_scripts');
+function sc_urp_load_styles_scripts(){
+
+
+    //slider
+    wp_enqueue_style('sc_urp_slider_css',plugins_url() . '/ultimate-recent-posts/lib/slider/bjqs-1.3.css',false, '1.3');
+    wp_enqueue_script('sc_urp_slider_js',plugins_url() . '/ultimate-recent-posts/lib/slider/bjqs-1.3.min.js',array('jquery'),'1.3');
+
+
+    // plugin main style
+    wp_enqueue_style('sc_urp_default_style',plugins_url() . '/ultimate-recent-posts/style/default.css',false, '1.0');
+
+    // plugin main script
+    wp_enqueue_script('sc_urp_default_script',plugins_url() . '/ultimate-recent-posts/script/sc_urp_script.js',array('jquery'), '1.0');
+
 
 }
 
